e4b7572a24142bf20ca7d487b54e665945c1fa82
Travis Horn <travis@travishorn.com>
2018-10-27 11:42:20 -0500

    Provide sample data to public

--- src/components/FileSelector.vue
+++ src/components/FileSelector.vue
@@ -2,16 +2,22 @@
   <div>
     <h2>Select file</h2>
     <input type="file" accept=".csv" @input="fileChange">
+    <SampleData />
   </div>
 </template>
 
 <script>
+import SampleData from './SampleData.vue';
+
 export default {
   name: 'FileSelector',
   methods: {
     fileChange(e) {
       this.$store.dispatch('changeFile', e.target.files[0]);
     },
+  },
+  components: {
+    SampleData,
   },
 };
 </script>
---
+++ src/components/SampleData.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <small>
+      Download sample data:
+      <a class="btn btn-sm btn-light mr-2" href="/sample-data/english-letter-frequency.csv">english-letter-frequency.csv</a>
+      <a class="btn btn-sm btn-light" href="/sample-data/world-population.csv">world-population.csv</a>
+    </small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SampleData',
+};
+</script>
