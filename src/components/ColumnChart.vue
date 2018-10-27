74e38d70cb75a4897f9eaa4eb46bf85070b230ac
Travis Horn <travis@travishorn.com>
2018-10-27 09:45:00 -0500

    Add D3. Placeholder axes

--- src/components/ColumnChart.vue
+++ src/components/ColumnChart.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import * as d3 from 'd3';
+
 export default {
   name: 'ColumnChart',
   data() {
@@ -11,5 +13,40 @@ export default {
       height: 500,
     };
   },
+  mounted() {
+    const margin = {
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50,
+    };
+
+    const chart = d3.select(this.$el);
+
+    const x = d3.scaleBand()
+      .domain(['One', 'Two', 'Three'])
+      .range([margin.left, this.width - margin.right])
+      .padding(0.1);
+
+    const y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([this.height - margin.bottom, margin.top]);
+
+    const xAxis = g => g
+      .attr('transform', `translate(0, ${this.height - margin.bottom})`)
+      .call(d3.axisBottom(x)
+        .tickSizeOuter(0));
+
+    const yAxis = g => g
+      .attr('transform', `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select('.domain').remove());
+
+    chart.append('g')
+      .call(xAxis);
+
+    chart.append('g')
+      .call(yAxis);
+  }
 };
 </script>
